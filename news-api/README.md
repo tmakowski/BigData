374ed785005dbc981f31fb472a38ff89da64c94c
tmakowski <tymek.makowski@gmail.com>
2019-03-21 15:01:23 +0100

    scrapery: NYT, USA Today, Bussiness Insider

--- news-api/README.md
+++ news-api/README.md
@@ -2,7 +2,9 @@
 
 Klasa `NewsAPI` jest gotowa i dość szczegółowo opisana, więc zrozumienie co tam się dzieje nie powinno być problemem. Główna metoda to `watch`, która na bieżąco pobiera info z api na temat podanego słowa kluczowego.
 
-Obsługiwane serwisy (gotowe scrapery): bbc-news, cnn
+Obsługiwane serwisy (gotowe scrapery): status jest w pliku `scrapers_todo.csv`
+
+**UWAGA:** trzeba uważać na kodowanie w wynikowych csv-kach.
 
 ## ToDo
 - [ ] funkcja do pobierania przeszłych artykułów
--- news-api/scrapers.py
+++ news-api/scrapers.py
@@ -8,11 +8,20 @@ def get_page(url):
     return page
 
 
-def get_content_bbc(url):
+def get_content_bbcnews(url):
     page = get_page(url)
     return " ".join([
         el.text
         for el in page.find("div[class='story-body'] div[property='articleBody'] p")
+        if el.text is not None
+    ])
+
+
+def get_content_bussinessinsider(url):
+    page = get_page(url)
+    return " ".join([
+        el.text
+        for el in page.find("div[data-piano-inline-content-wrapper] p")
         if el.text is not None
     ])
 
@@ -26,7 +35,28 @@ def get_content_cnn(url):
     ])
 
 
+def get_content_thenewyorktimes(url):
+    page = get_page(url)
+    return " ".join([
+        el.text
+        for el in thenewyorktimes_page.find("section[name='articleBody'] p")
+        if el.text is not None
+    ])
+
+
+def get_content_usatoday(url):
+    page = get_page(url)
+    return " ".join([
+        el.text
+        for el in page.find("article div[class='article-wrapper'] p")
+        if el.text is not None
+    ])
+
+
 SCRAPER_DICT = {
-    "bbc": get_content_bbc,
-    "cnn": get_content_cnn
+    "bbc-news": get_content_bbcnews,
+    "business-insider": get_content_bussinessinsider,
+    "cnn": get_content_cnn,
+    "the-new-york-times": get_content_thenewyorktimes,
+    "usa-today": get_content_usatoday
 }
