9ae87141bb2df65b8e4d7cc26ab9c766e343fde6
tmakowski <tymek.makowski@gmail.com>
2019-03-23 07:36:38 +0100

    5 nowych scraperów

--- news-api/scrapers.py
+++ news-api/scrapers.py
@@ -8,13 +8,52 @@ def get_page(url):
     return page
 
 
+def get_content_abcnews(url):
+    page = get_page(url)
+    return " ".join([
+        el.text
+        for el in page.find("div[id='articleBody'] p")
+        if el.text is not None])
+
+
+def get_content_abcnewsau(url):
+    page = get_page(url)
+    return " ".join([
+        el.text
+        for el in page.find("div[class='article section'] p")
+        if el.text is not None])
+
+
+def get_content_aljazeeraenglish(url):
+    page = get_page(url)
+    return " ".join([
+        el.text
+        for el in page.find("div[class='article-p-wrapper'] p")
+        if el.text is not None])
+
+
+def get_content_arstechnica(url):
+    page = get_page(url)
+    return " ".join([
+        el.text
+        for el in page.find("div[itemprop='articleBody'] p,h2")
+        if el.text is not None])
+
+
+def get_content_associatedpress(url):
+    page = get_page(url)
+    return " ".join([
+        el.text
+        for el in page.find("div[class='Article'] p")
+        if el.text is not None])
+
+
 def get_content_bbcnews(url):
     page = get_page(url)
     return " ".join([
         el.text
         for el in page.find("div[class='story-body'] div[property='articleBody'] p")
-        if el.text is not None
-    ])
+        if el.text is not None])
 
 
 def get_content_bussinessinsider(url):
@@ -22,8 +61,7 @@ def get_content_bussinessinsider(url):
     return " ".join([
         el.text
         for el in page.find("div[data-piano-inline-content-wrapper] p")
-        if el.text is not None
-    ])
+        if el.text is not None])
 
 
 def get_content_cnn(url):
@@ -31,16 +69,14 @@ def get_content_cnn(url):
     return " ".join([
         el.text
         for el in page.find("div[itemprop='articleBody'] [class^='zn-body__paragraph']")
-        if el.text is not None
-    ])
+        if el.text is not None])
 
 
 def get_content_foxnews(url):
     page = get_page(url)
     return " ".join([el.text
         for el in page.find("div[class='article-body'] > p")
-        if el.text is not None and el.find("strong", first=True) is None # Pomijamy nagłówki innych artykułów wplecione w tekst -- są one pogrubiane
-    ])
+        if el.text is not None and el.find("strong", first=True) is None])  # Pomijamy nagłówki innych artykułów wplecione w tekst -- są one pogrubiane
 
 
 def get_content_thenewyorktimes(url):
@@ -48,8 +84,7 @@ def get_content_thenewyorktimes(url):
     return " ".join([
         el.text
         for el in thenewyorktimes_page.find("section[name='articleBody'] p")
-        if el.text is not None
-    ])
+        if el.text is not None])
 
 
 def get_content_usatoday(url):
@@ -57,11 +92,15 @@ def get_content_usatoday(url):
     return " ".join([
         el.text
         for el in page.find("article div[class='article-wrapper'] p")
-        if el.text is not None
-    ])
+        if el.text is not None])
 
 
 SCRAPER_DICT = {
+    "abc-news": get_content_abcnews,
+    "abc-news-au": get_content_abcnewsau,
+    "al-jazeera-english": get_content_aljazeeraenglish,
+    "ars-technica": get_content_arstechnica,
+    "associated-press": get_content_associatedpress,
     "bbc-news": get_content_bbcnews,
     "business-insider": get_content_bussinessinsider,
     "cnn": get_content_cnn,
