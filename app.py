9157e3d75fb2f1f8d8590b8ab87e27052175a354
tmakowski <tymek.makowski@gmail.com>
2019-05-24 11:42:42 +0200

    Updated intervals

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-from datetime import datetime, timedelta
-
+# Dash imports
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-import pandas as pd
+# Plotly imports
 import plotly.graph_objs as go
+
+# Own modules imports
+from alphavantage.utils import next_update_time
+
+# Other imports
+from datetime import datetime
+import pandas as pd
 import os
+
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -22,12 +29,12 @@ app.layout = html.Div([
         
     # Defining intervals
     dcc.Interval(
-        id='interval-plot', n_intervals=0,
-        interval=1000*60),  # in milliseconds
-    
-    dcc.Interval(
         id='interval-clock', n_intervals=0,
-        interval=1000*1)  # in milliseconds
+        interval=1000*1),  # in milliseconds
+
+    dcc.Interval(
+        id='interval-plot', n_intervals=0,
+        interval=1000*5)  # in milliseconds
 ])
 
 
@@ -47,21 +54,20 @@ def live_clock(n):
 def live_next_update(n):
     """ Function displays time when next update will be displayed. """
     global STYLE
-    current_time = datetime.now()
     return html.Span(
-        "Next update: %s" % (current_time + timedelta(minutes=(5 - current_time.minute % 5))).strftime("%H:%M:00"),
+        "Next update: %s (possible delay: 5 seconds)" % next_update_time(offset=5, format_output=True),
         style=STYLE)
 
 
 @app.callback(Output("live-plot", "figure"), [Input("interval-plot", "n_intervals")])
 def live_plot(n):
     """ Function displays live plot. """
-    
+
     # Loading stock data
-    stock_data_path = os.path.join("..", "alphavantage", "tesla_prices.csv")
+    stock_data_path = os.path.join("alphavantage", "tesla_prices.csv")
     assert os.path.isfile(stock_data_path)
     stock_data = pd.read_csv(stock_data_path)
-    
+
     # Defining the stock candle plot trace
     trace_candle = go.Candlestick(
         x=pd.to_datetime(stock_data.loc[:, "timestamp"]),
@@ -70,17 +76,17 @@ def live_plot(n):
         low=stock_data.loc[:, "low"],
         close=stock_data.loc[:, "close"]
     )
-    
+
     # Plot parameters
     plot_layout = go.Layout(
         title="Stock prices",
         yaxis={"title": "Price (USD)"})
-    
+
     # Defining plotly figure
     plot_fig = go.Figure(
         data=[trace_candle],
         layout=plot_layout)
-   
+
     return plot_fig
 
 
