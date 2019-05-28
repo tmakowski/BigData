497690479126ca0f5dbf30ba2c9d6ace50c5860d
tmakowski <tymek.makowski@gmail.com>
2019-05-28 21:01:07 +0200

    Data mode, model output highliter

--- app.py
+++ app.py
@@ -11,10 +11,12 @@ import plotly.graph_objs as go
 from alphavantage.utils import curr_time, next_update_time
 
 # Other imports
+from datetime import timedelta
 import pandas as pd
 import os
 
 
+UPDATE_OFFSET = 5
 CLOCK_STYLE = {
     "display": "inline-block",
     "width": "14%",
@@ -26,6 +28,10 @@ CLOCK_STYLE = {
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div([
+    # Defining intervals (in milliseconds)
+    dcc.Interval(id='interval-clock', n_intervals=0, interval=1000*1),
+    dcc.Interval(id='interval-plot', n_intervals=0, interval=1000*5),
+    
     # Local time clock
     html.Div([
         html.Div("Local time"), html.Div(id="live-clock-local"),
@@ -33,20 +39,24 @@ app.layout = html.Div([
     ], style=CLOCK_STYLE),
 
     # Gouda Praga
-    html.H3("🔥 Gouda Praga 🔥", style={"width": "69%", "display": "inline-block", "textAlign": "center"}),
+    html.Div([
+        html.H3(id="model-output"),
+    ], style={"width": "69%", "display": "inline-block", "textAlign": "center"}),
 
     # Stock market time clock
     html.Div([
         html.Div("Stock market time"), html.Div(id="live-clock-us"),
         html.Div("Next update at"), html.Div(id="live-next-update-us")
     ], style=CLOCK_STYLE),
-
+    
+    # Choose mode
+    dcc.Dropdown(id="plot-mode-dropdown", options=[
+        {"label": "Last 24h", "value": 1},
+        {"label": "Historic data", "value": 0}
+    ], value=1),
+    
     # Plot
-    dcc.Graph(id="live-plot"),
-        
-    # Defining intervals (in milliseconds)
-    dcc.Interval(id='interval-clock', n_intervals=0, interval=1000*1),
-    dcc.Interval(id='interval-plot', n_intervals=0, interval=1000*5)
+    dcc.Graph(id="live-plot")
 ])
 
 
@@ -60,7 +70,7 @@ def live_clock_local(n):
 
 @app.callback(Output("live-next-update-local", "children"), [Input("interval-clock", "n_intervals")])
 def live_next_update_local(n):
-    return html.Span(next_update_time(offset=5, output_format="%d.%m.%Y, %H:%M:%S"))
+    return html.Span(next_update_time(offset=UPDATE_OFFSET, output_format="%d.%m.%Y, %H:%M:%S"))
 
 
 # ------------------------------------------------------------
@@ -73,20 +83,25 @@ def live_clock_est(n):
 
 @app.callback(Output("live-next-update-us", "children"), [Input("interval-clock", "n_intervals")])
 def live_next_update_us(n):
-    return html.Span(next_update_time(offset=5, output_format="%d.%m.%Y, %H:%M:%S", us_tz=True))
+    return html.Span(next_update_time(offset=UPDATE_OFFSET, output_format="%d.%m.%Y, %H:%M:%S", us_tz=True))
 
 
 # ----------------------------------------------
 # -------------------- Plot --------------------
 # ----------------------------------------------
-@app.callback(Output("live-plot", "figure"), [Input("interval-plot", "n_intervals")])
-def live_plot(n):
+@app.callback(Output("live-plot", "figure"), [Input("interval-plot", "n_intervals"), Input("plot-mode-dropdown", "value")])
+def live_plot(n, mode):
     """ Function displays live plot. """
 
     # Loading stock data
     stock_data_path = os.path.join("alphavantage", "tesla_prices.csv")
     assert os.path.isfile(stock_data_path)
     stock_data = pd.read_csv(stock_data_path)
+    
+    # Last 24h
+    if mode:
+        stock_data["timestamp"] = pd.to_datetime(stock_data.loc[:, "timestamp"])
+        stock_data = stock_data[stock_data.timestamp >= curr_time(us_tz=True).replace(tzinfo=None) - timedelta(hours=24)]
 
     # Defining the stock candle plot trace
     trace_candle = go.Candlestick(
@@ -100,7 +115,8 @@ def live_plot(n):
     # Plot parameters
     plot_layout = go.Layout(
         title="Stock prices",
-        yaxis={"title": "Price (USD)"})
+        yaxis={"title": "Price (USD)"},
+        uirevision=mode)
 
     # Defining plotly figure
     plot_fig = go.Figure(
@@ -110,5 +126,14 @@ def live_plot(n):
     return plot_fig
 
 
+# ------------------------------------------------------
+# -------------------- Model output --------------------
+# ------------------------------------------------------
+@app.callback(Output("model-output", "children"), [Input("interval-plot", "n_intervals"), Input("plot-mode-dropdown", "value")])
+def model_output_display(n, model_prediction):
+    return html.H3("🔥 Gouda Praga 🔥",
+                   style={"background-color": "green" if model_prediction else "red"})
+                   
+    
 if __name__ == "__main__":
     app.run_server(debug=True)
